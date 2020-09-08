0b7ecb8c97a82ee3abb52dd286148f3d4618c042
chumakovvchuma <v.chumakovv@gmail.com>
2020-09-08 18:55:08 +0300

    init

---
+++ be/src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module, ValidationPipe } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { GraphqlOptions } from './graphql.options';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { APP_PIPE } from '@nestjs/core';
+import { PostModule } from './post/post.module';
+import { UserModule } from './user/user.module';
+import { AppController } from './app.controller';
+
+@Module({
+  imports: [
+    GraphQLModule.forRootAsync({
+      useClass: GraphqlOptions,
+    }),
+    PrismaModule,
+    AuthModule,
+    PostModule,
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+  ],
+})
+export class AppModule {}
